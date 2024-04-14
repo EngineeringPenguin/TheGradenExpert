d2710abe26f13f4f9f57e46638a8cc958a3c9ee3
mapleS12 <124668905+mapleS12@users.noreply.github.com>
2024-04-14 12:23:55 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ page = """
 
 #   GARDENEXPERT
 
-# All Location Visulaization
-<|{data}|chart|mode=markers|x=Time|y[1]=Temperature|y[2]=Humidity|y[3]=Temperature2|y[4]=Humidity2|>
+# Scatterplot of all Nodes
+<|{data_all}|chart|mode=markers|x=Time|y[1]=Temperature|y[2]=Humidity|y[3]=Temperature2|y[4]=Humidity2|>
 
-# Side By Side Overlay
-<|{data}|chart|mode=none|x=Time|y[1]=Temperature|y[2]=Humidity|options={options}|>
-
-# Side By Side Overlay
+# Node 1 Temperature vs Humidity
 <|{data_one}|chart|mode=none|x=Time|y[1]=Temperature|y[2]=Humidity|options={options}|>
+
+# Node 2 Temperature vs Humidity
+<|{data_two}|chart|mode=none|x=Time|y[1]=Temperature|y[2]=Humidity|options={options}|>
+
+# Senario Sudden Change
+<|{bad_data}|chart|mode=none|x=Time|y[1]=Humidity|y[2]=Temperature|options={options}|>
 """
 
 humidityVal=4;
@@ -24,8 +27,10 @@ def get_data(path: str):
     dataset= pd.read_csv(path)
     return dataset
 
-data = get_data("temp_vals.csv")
-data_one = get_data("temp_vals2.csv")
+data_one = get_data("temp_vals1.csv")
+data_all = get_data ("temp_valsA.csv")
+data_two = get_data ("temp_vals3.csv")
+bad_data = get_data ("val_overlap.csv")
 
 
 options = [
